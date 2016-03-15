d9da4470f430abf4a9a66c5a4377cbd90444b49b
darrenCoding <1542079459@qq.com>
2016-03-15 18:23:50 +0800

    fix a bug

---
+++ bin/push.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const child_process = require('child_process');
+const path = require('path');
+const words = process.argv[2];
+
+
+let comment = words || 'fix a bug';
+
+/*const command = child_process.spawn(
+        'git',['add','.'],
+        {cwd: path.resolve(__dirname, '..')});*/
+const command = child_process.exec(
+        'sudo git add . && sudo git commit -m "fix a bug" ',
+        {cwd: path.resolve(__dirname, '..')},function(err,stdout,stderr){
+        	if(err){
+        		return console.log(err);
+        	}
+        	console.log(stdout);
+        	console.log(stderr);
+        });
+
+command.stdout.on('data', function (data) {
+    console.log(data + '');
+})
+
+command.stdout.on('error', function (data) {
+    console.log(data + '');
+})
