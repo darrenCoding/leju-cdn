34de45b92c869749951c89f6a64a1c7f7566070c
darrenCoding <1542079459@qq.com>
2016-03-15 18:28:31 +0800

    fix a bug

--- bin/push.js
+++ bin/push.js
@@ -9,12 +9,10 @@ const words = process.argv[2];
 
 let comment = words || 'fix a bug';
 
-/*const command = child_process.spawn(
-        'git',['add','.'],
-        {cwd: path.resolve(__dirname, '..')});*/
 const command = child_process.exec(
-        'sudo git add . && sudo git commit -m "fix a bug" ',
-        {cwd: path.resolve(__dirname, '..')},function(err,stdout,stderr){
+        'sudo git add . && sudo git commit -m "fix a bug" && sudo npm publish',
+        {cwd: path.resolve(__dirname, '..')},
+        function(err,stdout,stderr){
         	if(err || stderr){
         		return console.log(err);
         	}
