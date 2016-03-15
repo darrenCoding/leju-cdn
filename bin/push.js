5ddc8635e0704ed1503a17a667f41f7bcda7d399
darrenCoding <1542079459@qq.com>
2016-03-15 18:26:16 +0800

    fix a bug

--- bin/push.js
+++ bin/push.js
@@ -15,17 +15,11 @@ let comment = words || 'fix a bug';
 const command = child_process.exec(
         'sudo git add . && sudo git commit -m "fix a bug" ',
         {cwd: path.resolve(__dirname, '..')},function(err,stdout,stderr){
-        	if(err){
+        	if(err || stderr){
         		return console.log(err);
         	}
-        	console.log(stdout + "1111");
-        	/*console.log(stderr + "2222");*/
         });
 
 command.stdout.on('data', function (data) {
-    console.log(data + '333');
-})
-
-command.stdout.on('error', function (data) {
-    console.log(data + '444');
+    console.log(data);
 })
