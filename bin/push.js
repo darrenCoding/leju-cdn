757da1e6fd96c85e4f083370083620afd3080fe8
darrenCoding <1542079459@qq.com>
2016-12-20 22:58:21 +0800

    rm files

--- bin/push.js
+++ bin/push.js
@@ -12,12 +12,12 @@ let comment = words || 'fix a bug';
 const command = child_process.exec(
         'sudo git add . && sudo git commit -m "' + comment +'" && sudo git push origin master && sudo npm publish',
         {cwd: path.resolve(__dirname, '..')},
-        function(err,stdout,stderr){
-        	if(err || stderr){
+        (err, stdout, stderr) => {
+        	if ( err || stderr ) {
         		return console.log(err);
         	}
         });
 
-command.stdout.on('data', function (data) {
+command.stdout.on('data', (data) => {
     console.log(data);
 })
--- db.js
+++ db.js
@@ -1,9 +1,9 @@
-const redis = require('redis');
+const redis  = require('redis');
 const config = require('./config');
 const log4js = require('./config/log');
 const client = redis.createClient(config.rport, config.rhost);
 
-client.on('error', function(err, data) {
+client.on('error', (err, data) => {
     log4js.logger_e.error('redis error:' + err.stack || err.message);
 });
 
